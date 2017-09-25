fbb9d8368c454d60e7fdee383413301d7174b22d
Lyle Innes <lyleinnes@Lyles-MacBook-Pro.local>
2017-09-25 17:16:04 +1000

    done wk5monday homework

--- Lyle Innes/wk04/4-thu/movies_one/app.rb
+++ Lyle Innes/wk04/4-thu/movies_one/app.rb
@@ -3,26 +3,32 @@ require 'sinatra/reloader'
 require 'pry'
 require 'httparty'
 
-
 get '/about' do
-  @movie = params['movie']
+  @movie = params[:title]
+  oneResponse = HTTParty.get("http://omdbapi.com/?t=#{@movie}&apikey=#{ENV['OMDB_API_KEY']}")
 
-  response = HTTParty.get("http://omdbapi.com/?t=#{@movie}&apikey=#{ENV['OMDB_API_KEY']}")
 
-
-  @title = response["Title"]
-  @released = response["Released"]
-  @director = response["Director"]
-  @actors = response["Actors"]
-  @runtime = response["Runtime"]
-  @rated = response["Rated"]
-  @poster = response["Poster"]
-  @plot = response["Plot"]
+  @title = oneResponse["Title"]
+  @released = oneResponse["Released"]
+  @director = oneResponse["Director"]
+  @actors = oneResponse["Actors"]
+  @runtime = oneResponse["Runtime"]
+  @rated = oneResponse["Rated"]
+  @poster = oneResponse["Poster"]
+  @plot = oneResponse["Plot"]
   # binding.pry
   erb :about
 end
 
-
 get '/' do
   erb :index
 end
+
+get '/results' do
+  @user_input = params['title_input']
+  response = HTTParty.get("http://omdbapi.com/?s=#{@user_input}&apikey=#{ENV['OMDB_API_KEY']}")
+  @movies = response.parsed_response["Search"]
+
+  erb :results
+end
+
